4507b1b48d852dfa99a26e80d67a52475a6966ad
Sam Zhang <sam.zijing.zhang@gmail.com>
2025-03-15 22:42:20 -0400

    update

---
+++ src/persona.ts
@@ -0,0 +1,44 @@
+export const persona = {
+  firstName: "Sam",
+  secondName: "Zhang",
+  description: "Undergraduate Student, Software Developer",
+  email: "sam.zijing.zhang [at] gmail [dot] com",
+  story:
+    "My name is Sam, I have been an undergraduate student at McGill University since Fall 2022, joint majoring in Computer Science and Statistics. I am currently working as a Software Development Intern at Coveo as part of their Machine Learning Platform Infrastructure team",
+  sections: {
+    experience: [
+      {
+        title: "Software Developer Intern @ Coveo",
+        desc: "jan. 2024 - apr. 2024",
+        url: "https://www.coveo.com",
+      },
+      {
+        title: "Software Developer Intern @ Dayforce",
+        desc: "sept. 2023 - apr. 2023",
+        url: "https://www.dayforce.com",
+      },
+      {
+        title: "Machine Learning Research Engineer Intern @ Meituan",
+        desc: "may 2023 - aug. 2023",
+        url: "https://www.meituan.com",
+      },
+    ],
+    projects: [
+      {
+        title: "mcgill.courses",
+        desc: "open-source, ad-free search and review platform for courses offered and instructors teaching at McGill University.",
+        url: "https://mcgill.courses",
+      },
+    ],
+  },
+  contacts: [
+    {
+      title: "Linkedin",
+      url: "https://www.linkedin.com/in/zhang-sam/",
+    },
+    {
+      title: "GitHub",
+      url: "https://github.com/SamZhang02",
+    },
+  ],
+};
